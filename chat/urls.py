d4642c8442b9c5c3cff0dddfd1ccaba0cb93b497
Soumita Paul <paulsoumita01@gmail.com>
2021-07-08 09:46:21 +0530

    message loading

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('<str:room>/',views.room,name='room'),
     path('checkroom',views.checkRoom,name='checkRoom'),
+    path('getmessage/<str:room>/',views.getMessages,name='getMessages'),
+    path('send',views.sendMessage,name='sendMessage'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Room,Message
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -21,3 +21,17 @@ def checkRoom(request):
         newroom=Room.objects.create(name=room)
         newroom.save()
     return redirect('/'+room+'/?username='+user)
+
+def sendMessage(request):
+    text=request.POST['message']
+    room_id = request.POST['room_id']
+    user=request.POST['username']
+
+    new_msg=Message.objects.create(value=text, username= user,room= room_id)
+    new_msg.save()
+    return HttpResponse("Message Sent!")
+
+def getMessages(request,room):
+    room_details=Room.objects.get(name=room)
+    messages=Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
